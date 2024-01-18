ef7147970b0b35387aa507eba5ae565e3ba16d35
unknown <valadaogithub@gmail.com>
2024-01-18 10:56:42 -0800

    arquivo main finalizado

--- main.py
+++ main.py
@@ -4,3 +4,21 @@ print("CalculadoraPY")
 print("________________")
 
 nmr1 = int(input("Digite um numero: "))
+nmr2 = int(input("Digite outro numero: "))
+
+op = int(input("Com qual opcao voce deseja operar? \n1- + \n2- - \n3- * \n4-/\n "))
+
+while (op >= 5) or (op <= 0):
+    op = int(input("Com qual opcao voce deseja operar? \n1- + \n2- - \n3- * \n4-/\n "))
+
+if op == 1:
+    soma(nmr1, nmr2)
+
+elif op == 2:
+    subtr(nmr1, nmr2)
+
+elif op ==3:
+    multp(nmr1, nmr2)
+
+else:
+    div(nmr1, nmr2)
